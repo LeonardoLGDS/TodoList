bd85a517720741b50e84d89700e83711d5be3e20
Leonardo DSSiedler <54513066+LeonardoLGDS@users.noreply.github.com>
2021-09-26 09:57:17 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-class todoItem {
-    constructor(status, description, title, dueDate, priority) {
-        this.status = status;
-        this.description = description;
-        this.title = title;
-        this.dueDate = dueDate;
-        this.priority = priority;
-    }
-    editStatus() {};
-    editContent() {};
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
 }
 
-let todo = new todoItem("Unstarted", "task decription");
-
-console.log(todo.status = "asd");
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
