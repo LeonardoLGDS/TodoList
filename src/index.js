9cd32c3b27521c436ec3ec93b1a0dc4e7fbb08e7
Leonardo DSSiedler <54513066+LeonardoLGDS@users.noreply.github.com>
2021-09-29 18:39:56 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,90 @@
+const columnUninitiated = document.getElementById("uninitiated");
+const columnInitiated = document.getElementById("initiated");
+const columnFinished = document.getElementById("finished");
+const myAddButton = document.getElementById("addButton");
+const myTitle = document.getElementById("title");
+const myDescription = document.getElementById("description");
+const myTaskTitle = document.getElementById("taskTitle");
+const myTaskDescription = document.getElementById("taskDescription");
+const indexAddItem = document.getElementById("indexAddTask");
+const auxDeleteItem = document.getElementById("deleteButton");
+const auxItemToNextColumn = document.getElementById("nextButton");
+let itemsCreatedObj = []; // Stores the Todo's object, as in a "database".
+
+
+
 function openForm() {
-  document.getElementById("myForm").style.display = "block";
+  indexAddTask.style.display = "block";
 }
 
 function closeForm() {
-  document.getElementById("myForm").style.display = "none";
+  indexAddTask.style.display = "none";
 }
 
-const containerUninitiated = document.getElementById("uninitiated");
-const myAddButton = document.getElementById("addButton");
-const myTitle = document.getElementById("title");
-const myDescription = document.getElementById("title");
+function openItem(id) {
+  document.getElementById("myTask").style.display = "block";
+  myTaskTitle.innerHTML = itemsCreatedObj[id].title;
+  myTaskDescription.value = itemsCreatedObj[id].description;
+  auxDeleteItem.setAttribute("onclick", `deleteItem(${id})`);
+  auxItemToNextColumn.setAttribute("onclick", `itemToNextColumn(${id})`);
+}
 
-class TaskTodo {
-	constructor (status, title, description) {
-		this.status = status;
-		this.title = title;
-		this.description = description;
+function deleteItem(id) {
+  document.getElementById("myTask").style.display = "none";
+  myTaskTitle.innerHTML = "";
+  myTaskDescription.value = "";
+  itemsCreatedObj[id] = undefined;
+  let itemRemoval = document.getElementById(`${id}`);
+  itemRemoval.remove();
+}
+
+function closeItem() {
+  document.getElementById("myTask").style.display = "none";
+  myTaskTitle.innerHTML = "";
+  myTaskDescription.value = "";
+}
+
+function itemToNextColumn(id) {
+	let itemMove = document.getElementById(`${id}`);
+	console.log(itemsCreatedObj[id].status)
+	if(itemsCreatedObj[id].status === "unstarted") {
+		itemsCreatedObj[id].status = "started";
+		columnInitiated.append(itemMove);
+	} else {
+		itemsCreatedObj[id].status = "finished";
+		columnFinished.append(itemMove);
 	}
 }
-let itemsCreatedNumber = [];
-let itemsCreatedObj = [];
+
+
+class TaskTodo {
+  constructor(status, title, description, dueDate, priority) {
+    this.status = status;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+
+myAddButton.onclick = () => createItemTodo();
+
 let createItemUniqueId = () => {
   let count = 0;
-  while (itemsCreatedNumber.includes(count)) {
+  while (itemsCreatedObj[count]) {
     ++count
   }
-  itemsCreatedNumber.push(count);
 
   return count;
 }
 
 function createItemTodo() {
   const newItem = document.createElement('div');
-	newItem.classList.add('items');
+  newItem.classList.add('items');
   newItem.setAttribute('id', createItemUniqueId())
-	itemsCreatedObj[newItem.id] = new TaskTodo("unstarted", myTitle.value,myDescription.value);
-	newItem.innerHTML = itemsCreatedObj[newItem.id].title;
-	containerUninitiated.append(newItem);
-}
+  itemsCreatedObj[newItem.id] = new TaskTodo("unstarted", myTitle.value, myDescription.value);
+  newItem.innerHTML = itemsCreatedObj[newItem.id].title;
+  columnUninitiated.append(newItem);
 
-myAddButton.onclick = () => createItemTodo();
+  newItem.setAttribute("onclick", `openItem(${newItem.id})`);
+}
