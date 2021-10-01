5e4e18636938628a7d2a09ec25731c6721cd932f
Leonardo DSSiedler <54513066+LeonardoLGDS@users.noreply.github.com>
2021-10-01 14:57:28 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,60 +2,19 @@ const columnUninitiated = document.getElementById("uninitiated");
 const columnInitiated = document.getElementById("initiated");
 const columnFinished = document.getElementById("finished");
 const myAddButton = document.getElementById("addButton");
+const myDescriptionAdd = document.getElementById("description");
 const myTitle = document.getElementById("title");
-const myDescription = document.getElementById("description");
-const myTaskTitle = document.getElementById("taskTitle");
 const myTaskDescription = document.getElementById("taskDescription");
 const indexAddItem = document.getElementById("indexAddTask");
-const auxDeleteItem = document.getElementById("deleteButton");
+const myTaskTitle = document.getElementById("taskTitle");
+const myTaskDueDateDetails = document.getElementById("dueDateDetails");
+const myTaskPriorityDetails = document.getElementById("priorityDetails");
+const myTaskDueDateAdd = document.getElementById("dueDateAdd");
+const myTaskPriorityAdd = document.getElementById("priorityAdd");
 const auxItemToNextColumn = document.getElementById("nextButton");
-let itemsCreatedObj = []; // Stores the Todo's object, as in a "database".
-
-
-
-function openForm() {
-  indexAddTask.style.display = "block";
-}
-
-function closeForm() {
-  indexAddTask.style.display = "none";
-}
-
-function openItem(id) {
-  document.getElementById("myTask").style.display = "block";
-  myTaskTitle.innerHTML = itemsCreatedObj[id].title;
-  myTaskDescription.value = itemsCreatedObj[id].description;
-  auxDeleteItem.setAttribute("onclick", `deleteItem(${id})`);
-  auxItemToNextColumn.setAttribute("onclick", `itemToNextColumn(${id})`);
-}
-
-function deleteItem(id) {
-  document.getElementById("myTask").style.display = "none";
-  myTaskTitle.innerHTML = "";
-  myTaskDescription.value = "";
-  itemsCreatedObj[id] = undefined;
-  let itemRemoval = document.getElementById(`${id}`);
-  itemRemoval.remove();
-}
-
-function closeItem() {
-  document.getElementById("myTask").style.display = "none";
-  myTaskTitle.innerHTML = "";
-  myTaskDescription.value = "";
-}
-
-function itemToNextColumn(id) {
-	let itemMove = document.getElementById(`${id}`);
-	console.log(itemsCreatedObj[id].status)
-	if(itemsCreatedObj[id].status === "unstarted") {
-		itemsCreatedObj[id].status = "started";
-		columnInitiated.append(itemMove);
-	} else {
-		itemsCreatedObj[id].status = "finished";
-		columnFinished.append(itemMove);
-	}
-}
-
+const auxDeleteItem = document.getElementById("deleteButton");
+const auxItemSaveEdit = document.getElementById("saveButton");
+let itemsCreatedObj = []; // Stores the Todo's objects, as in a "database".
 
 class TaskTodo {
   constructor(status, title, description, dueDate, priority) {
@@ -78,13 +37,103 @@ let createItemUniqueId = () => {
   return count;
 }
 
+function changePriorityAdd(element) {
+  if (element.innerHTML === "Low") {
+    myTaskPriorityAdd.style.backgroundColor = "green";
+    myTaskPriorityAdd.innerHTML = element.innerHTML;
+    myTaskPriorityAdd.value = element.innerHTML;
+  } else if (element.innerHTML === "Medium") {
+    myTaskPriorityAdd.style.backgroundColor = "#e6ac00";
+    myTaskPriorityAdd.innerHTML = element.innerHTML;
+    myTaskPriorityAdd.value = element.innerHTML;
+  } else {
+    myTaskPriorityAdd.style.backgroundColor = "red";
+    myTaskPriorityAdd.innerHTML = element.innerHTML;
+    myTaskPriorityAdd.value = element.innerHTML;
+  }
+}
+
+function openAddForm() {
+  indexAddTask.style.display = "block";
+}
+
+function closeAddForm() {
+  indexAddTask.style.display = "none";
+  myTitle.value = "";
+  myDescriptionAdd.value = "";
+  myTaskPriorityAdd.innerHTML = "";
+  myTaskDueDateAdd.value = "";
+  myTaskPriorityAdd.style.backgroundColor = "green";
+}
+
+// See details of an already created Todo.
+function openItem(id) {
+  document.getElementById("myTask").style.display = "block";
+  myTaskTitle.innerHTML = itemsCreatedObj[id].title;
+  myTaskDescription.value = itemsCreatedObj[id].description;
+  auxDeleteItem.setAttribute("onclick", `deleteItem(${id})`);
+  auxItemToNextColumn.setAttribute("onclick", `itemToNextColumn(${id})`);
+  myTaskDueDateDetails.value = itemsCreatedObj[id].dueDate;
+  myTaskPriorityDetails.innerHTML = itemsCreatedObj[id].priority;
+
+  if (myTaskPriorityDetails.innerHTML === "Low") {
+    myTaskPriorityDetails.style.backgroundColor = "green";
+  } else if (myTaskPriorityDetails.innerHTML === "Medium") {
+    myTaskPriorityDetails.style.backgroundColor = "#e6ac00";
+  } else if (myTaskPriorityDetails.innerHTML === "High") {
+    myTaskPriorityDetails.style.backgroundColor = "red";
+  } else {
+    myTaskPriorityDetails.innerHTML = "Not defined";
+    myTaskPriorityDetails.style.backgroundColor = "grey"
+  }
+
+  if (itemsCreatedObj[id].status === "finished") {
+    auxItemToNextColumn.style.backgroundColor = "grey";
+  }
+}
+
+// Removes Todo from the layout and database.
+function deleteItem(id) {
+  document.getElementById("myTask").style.display = "none";
+  myTaskTitle.innerHTML = "";
+  myTaskDescription.value = "";
+  itemsCreatedObj[id] = undefined;
+  let itemRemoval = document.getElementById(`${id}`);
+  itemRemoval.remove();
+}
+
+// Close the "Add Todo" window.
+function closeItem() {
+  document.getElementById("myTask").style.display = "none";
+}
+
+// Moves the Todo trough the stages of completion.
+function itemToNextColumn(id) {
+  let itemMove = document.getElementById(`${id}`);
+  console.log(itemsCreatedObj[id].status)
+  if (itemsCreatedObj[id].status === "unstarted") {
+    itemsCreatedObj[id].status = "started";
+    columnInitiated.append(itemMove);
+  } else {
+    itemsCreatedObj[id].status = "finished";
+    columnFinished.append(itemMove);
+    auxItemToNextColumn.style.backgroundColor = "grey";
+  }
+}
+
+// Creates the Todo with the information contained on the "Add" window.
 function createItemTodo() {
   const newItem = document.createElement('div');
-  newItem.classList.add('items');
-  newItem.setAttribute('id', createItemUniqueId())
-  itemsCreatedObj[newItem.id] = new TaskTodo("unstarted", myTitle.value, myDescription.value);
-  newItem.innerHTML = itemsCreatedObj[newItem.id].title;
-  columnUninitiated.append(newItem);
-
-  newItem.setAttribute("onclick", `openItem(${newItem.id})`);
+  if (myTitle.value === "") {
+    document.getElementById("addTaskErrorMessageTitle").innerHTML = "*Title cannot be empty!*";
+  } else {
+    newItem.classList.add('items');
+    newItem.setAttribute('id', createItemUniqueId())
+    itemsCreatedObj[newItem.id] = new TaskTodo("unstarted", myTitle.value, myDescriptionAdd.value, myTaskDueDateAdd.value, myTaskPriorityAdd.value);
+    newItem.innerHTML = itemsCreatedObj[newItem.id].title;
+    columnUninitiated.append(newItem);
+    console.log(itemsCreatedObj[newItem.id]);
+    newItem.setAttribute("onclick", `openItem(${newItem.id})`);
+    closeAddForm();
+  }
 }
